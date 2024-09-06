ff337364ae8f436ef1694bdea4ae79fb74987dd3
AhmedDR200 <alshwwhy212@gmail.com>
2024-09-06 19:14:38 +0300

    Setup Express Server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+
+// Load env vars
+dotenv.config();
+
+const app = express();
+
+// body parser
+app.use(express.json());
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// Server Connection
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(
+    `server (${process.env.NODE_ENV}) listening at http://localhost:${port}`
+      .yellow.bold
+  );
+});
